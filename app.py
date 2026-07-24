545089e74eeac0b80ecb71aa1d2374cb924dd00c
shrvnaii <sawantshravani2129@gmail.com>
2026-07-24 18:12:31 +0530

    Reorder import statements in app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-from rdkit import Chem
-from rdkit.Chem import Descriptors, AllChem
-import py3Dmol
-from stmol import showmol
 import urllib.request
 import ssl
+import py3Dmol
+from stmol import showmol
 
 st.title("In Silico Drug Tester")
 st.write("Analyze small molecules for bioavailability and visualize 3D structures.")
